439db014c9d9806c84a9861b40ab1c60ee43b8bc
kumarAnunay <kumaranunay12@gmail.com>
2023-10-19 10:38:45 +0530

    AnalyzedText css corrected

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import "./styles/index.css";
 import ErrorBoundary from "./Containers/ErrorBoundary/ErrorBoundary.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>
+  <ErrorBoundary>
+    <App />
+  </ErrorBoundary>
 );
